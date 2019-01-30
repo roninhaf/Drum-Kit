7e99d14368dc38d43dd8fd66d2136d006d01ff7b
roninhaf <roninhaf@gmail.com>
2019-01-29 19:48:27 -0500

    upload last changes

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log();
+function removePlayingClass(a) {
+    if(a.propertyName!=='transform') {return;};console.log(a.target.tagName);
+    a.target.classList.remove('playing');
+}
+
+function playsound(a) {
+    const audio = document.querySelector(`audio[data-key="${a.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${a.keyCode}"]`);
+    if(!audio){return};
+    //console.log(key);
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));console.log(keys);
+keys.forEach(a => a.addEventListener('transitionend', removePlayingClass));
+
+window.addEventListener('keydown',playsound);
